6e4a74ed7ce564b7253e12fad94e52da8cfc504a
Jarvis Yuan <jarviskroos7@berkeley.edu>
2020-10-17 16:30:21 -0700

    update with day of week functionality

--- route_speed.py
+++ route_speed.py
@@ -1,4 +1,3 @@
-#import geopandas as gpd
 import argparse
 import pandas as pd
 import numpy as np
@@ -7,32 +6,30 @@ import os
 import sys
 
 import logging
-logger = logging.getLogger('script-logger')
-
-#from shapely.geometry import LineString
-#import pyproj
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
 import warnings
 warnings.filterwarnings("ignore")
-error_msg = 'failed to calculate route speed in provided GTFS feed with provided parameters: '
+error_msg = 'failed to calculate route speed: '
 
 # set default path to be the current directory where GTFS feed is assumed to be stored
 path_default = os.getcwd()
 
-def get_avg_route_speed(route_short_name=None, route_id=None, trip_id=None, path=path_default, hasShape=False, hour=None):
+def get_avg_route_speed(route_short_name=None, route_id=None, trip_id=None, path=path_default, hasShape=False, hour=None, day=None):
 
     os.chdir(path)
     try:
         routes = pd.read_csv('routes.txt')
         trips = pd.read_csv('trips.txt')
-        stops = pd.read_csv('stops.txt')
         stop_times = pd.read_csv('stop_times.txt')
+        calendar = pd.read_csv('calendar.txt')
         if hasShape == True:
             shapes = pd.read_csv('shapes.txt')
         else:
             pass
     except:
-        logger.error(error_msg, 'required feed files do not exist, check your feed before running the script')
+        #logging.error(error_msg, 'not all required feed files exist, check your feed or directory before running the script')
+        sys.exit(f"{error_msg} not all required feed files exist, check your feed or directory before running the script")
         return
 
     ## TODO: implement estimated route distance calculation with stops.txt only
@@ -40,16 +37,17 @@ def get_avg_route_speed(route_short_name=None, route_id=None, trip_id=None, path
         pass
 
     if type(route_short_name) is str and route_short_name != '':
-        print("for route", route_short_name, "...")
-        return speed_short_name(route_short_name, routes, trips, stop_times, shapes, hour)
+        print("- for bus route with route_short_name:", route_short_name, "...")
+        return speed_short_name(route_short_name, routes, trips, stop_times, shapes, calendar, hour, day)
     elif type(route_id) is np.int64:
-        print("for route_id", route_id, "...")
-        return speed_route_id(route_id, routes, trips, stop_times, shapes, hour)
+        print("- for bus route with route_id", route_id, "...")
+        return speed_route_id(route_id, routes, trips, stop_times, shapes, calendar, hour, day)
     elif type(trip_id) is str and trip_id != '':
-        print("for trip_id", trip_id, "...")
-        return speed_trip_id(trip_id, trips, stop_times, shapes)
+        print("- for trip_id", trip_id, "...")
+        return speed_trip_id(trip_id, trips, route_id, stop_times, shapes, calendar, day)
     elif route_short_name == None and route_id == None and trip_id == None:
-        logger.error(error_msg, 'missing valid trip/route identifier')
+        sys.exit(f"{error_msg} missing valid trip/route identifier")
+        #logging.error(error_msg, 'missing valid trip/route identifier')
         return
 
 def haversine_distance(lat1, lon1, lat2, lon2):
@@ -77,9 +75,20 @@ def haversine_distance(lat1, lon1, lat2, lon2):
 
     return res * 0.621371 # unit in km --> mile
 
-def speed_trip_id(trip_id, trips, stop_times, shapes):
+def speed_trip_id(trip_id, trips, route_id, stop_times, shapes, calendar, day):
     try:
         trips_route = trips.loc[trips['trip_id'] == trip_id]
+
+        if day in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
+            service_id = check_day_of_week(trips_route, trips, calendar, day, route_id, trip_id=trip_id)
+            trips_route = trips_route.loc[trips_route['service_id'] == service_id]
+        elif day == None or day == False:
+            pass
+        else:
+            #logging.error(error_msg, "invalid 'day' parameter was given.")
+            sys.exit(f"{error_msg} invalid 'day' parameter was given")
+            return
+
         shape_id = trips_route.shape_id.iloc[0]
         shapes_route = shapes.loc[shapes['shape_id'] == shape_id]
 
@@ -96,21 +105,20 @@ def speed_trip_id(trip_id, trips, stop_times, shapes):
             lat_prev = lat_curr
             lon_prev = lon_curr
 
-        print('* route distance =', distance, 'miles')
+        print('* route distance=', distance, 'miles')
         
         route_travel_time = get_route_travel_time(trip_id, stop_times)
 
         route_speed_avg = (distance / (route_travel_time / 60)) * 60
         print('.')
-        print('..')
-        print('...')
-        print('....')
-        print('.....')
-        print('--> avg route speed of bus route =', route_speed_avg, 'mph')
+        print('.')
+        print('.')
+        print('result: average route speed of specified bus route =', route_speed_avg, 'mph')
         return route_speed_avg
     except Exception as e:
         print(str(e))
-        logger.error(error_msg, 'input trip_id does not exist or invalid values are given')
+        #logging.error(error_msg, 'input trip_id does not exist or invalid values are given')
+        sys.exit(f"{error_msg} input trip_id does not exist or invalid values are given")
         return
 
 def get_route_travel_time(trip_id, stop_times):
@@ -130,13 +138,26 @@ def get_route_travel_time(trip_id, stop_times):
     return duration_in_s
 
 
-def speed_route_id(route_id, routes, trips, stop_times, shapes, hour):
+def speed_route_id(route_id, routes, trips, stop_times, shapes, calendar, hour, day):
 
+    
     try:
         trips_route = trips.loc[trips['route_id'] == route_id]
+
+        if day in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
+            service_id = check_day_of_week(trips_route, trips, calendar, day, route_id)
+            trips_route = trips_route.loc[trips_route['service_id'] == service_id]
+            day = False
+        elif day is None:
+            logging.info('no specific day of week was given, selecting trips at an arbitary available service')
+            pass
+        else:
+            #logging.error(error_msg, "invalid 'day' parameter was given.")
+            sys.exit(f"{error_msg} invalid 'day' parameter was given")
+
         trip_id_random = trips_route.reset_index().loc[0, 'trip_id'] # extract the first trip_id with input route_id for now
         if hour:
-            print('at hour: ', hour)
+            print('-- at hour:', hour)
             try:
                 merged = trips_route[['trip_id']].merge(stop_times[['trip_id', 'arrival_time']], how='left', on='trip_id')
                 merged['arrival_time'] = pd.to_datetime(merged['arrival_time'], format='%H:%M:%S', errors='coerce')
@@ -145,25 +166,44 @@ def speed_route_id(route_id, routes, trips, stop_times, shapes, hour):
                 trip_id_random = merged_trip_route.reset_index().loc[0, 'trip_id'] # extract the first trip_id with input route_id for now
                 print('actual departing time of trip', trip_id_random, ':', str(merged_trip_route.loc[merged_trip_route['trip_id'] == trip_id_random]['arrival_time'].dt.time.iloc[1]))
             except Exception as e:
-                logger.error(error_msg, 'no trips were available at input departing hour')
-                sys.exit()
+                #logging.error(error_msg, 'no trips were available at input departing hour')
+                sys.exit(f"{error_msg} no trips were available at hour {hour}")
+                return
         else:
-            print('! no specific hour were given, selecting trips at an arbitary departing time...')
-        return speed_trip_id(trip_id_random, trips_route, stop_times, shapes)
+            logging.info('no specific hour was given, selecting trips at an arbitary departing time...')
+        return speed_trip_id(trip_id_random, trips_route, route_id, stop_times, shapes, calendar, day)
     except Exception as e:
         print(str(e))
         return
     
 
-def speed_short_name(route_short_name, routes, trips, stop_times, shapes, hour):
+def speed_short_name(route_short_name, routes, trips, stop_times, shapes, calendar, hour, day):
 
     try:
         route_id = routes.loc[routes['route_short_name'] == route_short_name].route_id.iloc[0]
-        return speed_route_id(route_id, routes, trips, stop_times, shapes, hour)
+        return speed_route_id(route_id, routes, trips, stop_times, shapes, calendar, hour, day)
     except:
-        print(error_msg, 'input route_short_name does not exist or invalid values are given')
-        return
+        #logging.error(f"{error_msg} input route_short_name does not exist or invalid values are given")
+        sys.exit(f"{error_msg} input route_short_name {route_short_name} does not exist or invalid values are given")
 
+def check_day_of_week(trips_route, trips, calendar, day, route_id, trip_id=None):
+
+    status = False
+
+    for service_id in trips_route.service_id.unique():
+        service = calendar.loc[calendar['service_id'] == service_id]
+        
+        if service[day].iloc[0] == 1: # should only be returning on row here
+            print('-- service is available on', day, 'for route', route_id)
+            print('-- cauculating route speed on', day)
+            status = True
+            return service_id
+    if status == False:
+        if route_id:
+            sys.exit(f"no available service of route {route_id} is found on {day}")
+        else:
+            #logging.error(error_msg, 'no available service of trip', trip_id, 'is found on', day)
+            sys.exit(f"no available service of trip {trip_id} is found on {day}")
     
 
 
@@ -174,7 +214,8 @@ parser.add_argument("--route_id", "-rid", help="set route_id")
 parser.add_argument("--trip_id", "-tid", help="set trip_id")
 parser.add_argument("--path", "-p", help="set absolute path to the GTFS feed")
 parser.add_argument("--shape", "-s", help="tell the program if distance is to be calculated from shapes.txt")
-parser.add_argument("-t", "--time", help="calculate route speed at certain hour")
+parser.add_argument("-t", "--time", help="calculate route speed at certain hour, optional")
+parser.add_argument("-d", "--day", help="calculate route speed at given day of week, optional")
 parser.add_argument("-f", "--fff", help="a dummy argument to fool ipython", default="1")
 
 
@@ -184,36 +225,33 @@ route_short_name = None
 route_id = None
 trip_id = None
 time_in_hour = None
+day = None
 shape = False
-
-bar = '='
 
 
 if args.path:
     path_default = args.path
-    length = len(path_default) + len('- set absolute path to GTFS feeds to') + 1
-    barr = bar * length
-    print(barr)
-    print("GTFS route speed calculation script: ")
+    print("GTFS route speed calculation script: loaded!")
     print()
-    print("- set absolute path to GTFS feeds to %s" % args.path)
+    logging.info(f"- set absolute path to GTFS feeds to {args.path}")
 else:
-    length = len(path_default) + len('! no path was inputed, set absolute path to GTFS feeds to current directory') + 1
-    barr = bar * length
-    print(barr)
-    print("GTFS route speed calculation script: ")
-    print()
-    print("! no path was inputed, set absolute path to GTFS feeds to current directory %s" % path_default)
+    print("GTFS route speed calculation script: loaded!")
+    print(f"default feed path {os.getcwd()}")
+    logging.warning(f"no path was inputed, set absolute path to GTFS feeds to current directory {path_default}")
 
 if args.shape:
     shape = True
-    print("using provided shapes.txt: %s" % args.shape)
+    print("- using provided shapes.txt: %s" % args.shape)
 else:
-    print("! no shapes.txt exists in the GTFS feed provided, will be using stops.txt for straightline route disatnce approximation.")
+    logging.warning("no shapes.txt exists in the GTFS feed provided, will be using stops.txt for straightline route disatnce approximation")
 
 if args.time:
     time_in_hour = args.time
     print("- calculating route speed at hour: %s" % args.time)
+
+if args.day:
+    day = args.day
+    print("- calculating route speed operating on: %s" % args.day)
     
 if args.route_short_name:
     route_short_name = args.route_short_name
@@ -225,16 +263,13 @@ elif args.trip_id:
     trip_id = args.trip_id
     print("- set trip_id to %s" % args.trip_id)
 
-print(barr)
 
 
-def main(route_short_name, route_id, trip_id, path_default, shape, hour):
+def main(route_short_name, route_id, trip_id, path_default, shape, hour, day):
     return get_avg_route_speed(route_short_name=route_short_name, 
-    route_id=route_id, trip_id=trip_id, path=path_default, hasShape=shape, hour=hour)
+    route_id=route_id, trip_id=trip_id, path=path_default, hasShape=shape, hour=hour, day=day)
 
 
 
 if __name__ == "__main__":
-    main(route_short_name, route_id, trip_id, path_default, shape, time_in_hour)
-
-    
+    main(route_short_name, route_id, trip_id, path_default, shape, time_in_hour, day)   
